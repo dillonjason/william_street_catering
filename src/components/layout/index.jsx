a6dbf34e4a528f8d605a38d6008641d6cfeb37cf
Dillon Jason <djasuar@gmail.com>
2019-02-23 13:06:14 -0500

    Removed gatsby default stuff

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Header from "../header"
 import "./layout.css"
 
 const Layout = ({ children }) => (
@@ -28,11 +28,7 @@ const Layout = ({ children }) => (
           }}
         >
           <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
+          <footer>© {new Date().getFullYear()}, built by Dillon Jason</footer>
         </div>
       </>
     )}
