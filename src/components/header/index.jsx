bfcc316e38cb6685df8ecc08acbf980b49122206
Dillon Jason <djason@thefarmersdog.com>
2020-01-12 12:11:26 -0500

    Added navigation

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,10 @@
 import { graphql, Link, useStaticQuery } from "gatsby"
 import React from "react"
-import { FaPhone, FaEnvelope } from "react-icons/fa"
 
 import styles from "./styles.module.scss"
 
 import { Logo } from "../logo"
+import { Navigation } from "../navigation"
 
 const query = graphql`
   query {
@@ -21,26 +21,13 @@ export const Header = () => {
 
   return (
     <header className={styles.container}>
-      <div className={styles.main}>
-        <h1 className={styles.logo}>
-          <Link to="/">
-            <Logo />
-          </Link>
-        </h1>
-      </div>
-      <div className={styles.contact}>
-        <a className={styles.emailText} href={`mailto:${email}`}>
-          {email}
-        </a>
-        <a className={styles.emailIcon} href={`mailto:${email}`}>
-          <FaEnvelope size="1.5rem" />
-        </a>
-        <a className={styles.phoneText} href={`tel:${phone}`}>
-          {phone}
-        </a>
-        <a className={styles.phoneIcon} href={`tel:${phone}`}>
-          <FaPhone size="1.5rem" />
-        </a>
+      <h1 className={styles.logo}>
+        <Link to="/">
+          <Logo />
+        </Link>
+      </h1>
+      <div className={styles.navigation}>
+        <Navigation />
       </div>
     </header>
   )
