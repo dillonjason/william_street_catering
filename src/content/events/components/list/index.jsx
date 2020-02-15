077c564c5c6568d387400905c46dbda35144db2c
Dillon Jason <djason@thefarmersdog.com>
2020-02-15 12:33:36 -0500

    Added events list

---
+++ src/content/events/components/list/index.jsx
@@ -0,0 +1,58 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+
+import { Button } from "../../../../components/button"
+import { Article } from "../../../../components/article"
+
+import styles from "./styles.module.scss"
+
+const query = graphql`
+  query {
+    allSanityTestimonial(sort: { fields: date, order: DESC }) {
+      nodes {
+        date
+        title
+        type
+        slug {
+          current
+        }
+        summary
+        mainImage {
+          asset {
+            fluid(maxWidth: 1080) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export const List = () => {
+  const { allSanityTestimonial } = useStaticQuery(query)
+
+  return (
+    <>
+      {allSanityTestimonial.nodes.map((node, index) => (
+        <Article
+          key={node.slug.current}
+          title={`${node.title} ${node.type}`}
+          flip={index % 2}
+          className={styles.event}
+          img={
+            <Img
+              style={{ maxHeight: 700 }}
+              objectFit="cover"
+              fluid={node.mainImage.asset.fluid}
+            />
+          }
+        >
+          <p className={styles.summary}>{node.summary}</p>
+          <Button to={`/events/${node.slug.current}`}>Details</Button>
+        </Article>
+      ))}
+    </>
+  )
+}
